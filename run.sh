e8431345eddba893ba0ca51700336e8b7078c28d
zhewei <mvp159753@gmail.com>
2018-07-18 09:32:24 +0800

    add japanese model

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -O model.zip https://www.dropbox.com/s/fjr9rtupy7imx63/model.zip?dl=0
+wget -O model.zip https://www.dropbox.com/s/v8groisp39fkz3m/model.zip?dl=0
 unzip model.zip
 python app.py
